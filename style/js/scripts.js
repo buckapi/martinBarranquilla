9da3c709a03645fae4c054883a2cdddbced0a442
buckapi <buckapis@gmail.com>
2023-07-24 18:38:07 -0500

    ajustes

--- style/js/scripts.js
+++ style/js/scripts.js
@@ -1703,6 +1703,33 @@ $(document).ready(function() {
         // Set CSS color to display scrollUp active point, e.g '#00FFFF'
         zIndex: 1001 // Z-Index for the overlay
     });
+     $.scrollUpw({
+        scrollName: 'whatsapp',
+        // Element ID
+        scrollDistance: 300,
+        // Distance from top/bottom before showing element (px)
+        scrollFrom: 'top',
+        // 'top' or 'bottom'
+        scrollSpeed: 300,
+        // Speed back to top (ms)
+        easingType: 'linear',
+        // Scroll to top easing (see http://easings.net/)
+        animation: 'fade',
+        // Fade, slide, none
+        animationInSpeed: 200,
+        // Animation in speed (ms)
+        animationOutSpeed: 200,
+        // Animation out speed (ms)
+        scrollText: '<span class="btn btn-square btn-full-rounded btn-icon"><i class="fa fa-whatsapp"></i></span>',
+        // Text for element, can contain HTML
+        scrollTitle: false,
+        // Set a custom <a> title if required. Defaults to scrollText
+        scrollImg: false,
+        // Set true to use image
+        activeOverlay: false,
+        // Set CSS color to display scrollUp active point, e.g '#00FFFF'
+        zIndex: 1001 // Z-Index for the overlay
+    });
     /*-----------------------------------------------------------------------------------*/
     /*	PARALLAX MOBILE
     /*-----------------------------------------------------------------------------------*/
